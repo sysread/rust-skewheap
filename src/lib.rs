13decd6d13c511412801db00835a28053497bc8f
Jeff Ober <jober@ziprecruiter.com>
2021-12-04 18:26:55 -0500

    Not the prettiest, but at least this one compiles...

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,9 @@ impl SkewHeap {
     }
 
     fn put(&mut self, item: Item) -> u64 {
-        let node: Tree = Node::new(item, None, None);
-
-        self.root = match self.root {
-            None    => node,
-            Some(_) => Node::merge(self.root, node),
+        self.root = match &self.root {
+            Some(r) => Node::merge(&Some(r.clone()), &Node::new(item, None, None)),
+            None    => Node::new(item, None, None)
         };
 
         self.items += 1;
@@ -28,6 +26,7 @@ impl SkewHeap {
 type Item = i64;
 type Tree = Option<Box<Node>>;
 
+#[derive(Clone)]
 struct Node {
     item:  Item,
     left:  Tree,
@@ -39,15 +38,13 @@ impl Node {
         Some(Box::new(Node{ item: item, left: left, right: right }))
     }
 
-    fn merge(a: Tree, b: Tree) -> Tree {
-        let pair = (a, b);
-
-        match pair {
+    fn merge<'a>(a: &'a Tree, b: &'a Tree) -> Tree {
+        match (a,b) {
             (None,    None)                       => None,
-            (Some(a), None)                       => Some(a),
-            (None,    Some(b))                    => Some(b),
-            (Some(a), Some(b)) if a.item > b.item => Node::merge(Some(b), Some(a)),
-            (Some(a), Some(b))                    => Node::new(a.item, Node::merge(a.right, Some(b)), a.left),
+            (Some(a), None)                       => Some(a.clone()),
+            (None,    Some(b))                    => Some(b.clone()),
+            (Some(a), Some(b)) if a.item > b.item => Node::merge(&Some(b.clone()), &Some(a.clone())),
+            (Some(a), Some(b))                    => Node::new(a.item, Node::merge(&a.right, &Some(b.clone())).clone(), a.left.clone()),
         }
     }
 }
