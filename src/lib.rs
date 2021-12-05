cb5711dd828231d892e8e4f62d0607e543c25276
Jeff Ober <jober@ziprecruiter.com>
2021-12-04 19:03:51 -0500

    Add minimal positive path test

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
-struct SkewHeap {
+pub struct SkewHeap {
     items: u64,
     root:  Tree,
 }
 
 impl SkewHeap {
-    fn new() -> SkewHeap {
+    pub fn new() -> SkewHeap {
         SkewHeap{
             items: 0,
             root:  None,
         }
     }
 
-    fn put(&mut self, item: Item) -> u64 {
+    pub fn is_empty(&self) -> bool {
+        return self.items == 0
+    }
+
+    pub fn put(&mut self, item: Item) -> u64 {
         self.root = match &self.root {
             Some(r) => Node::merge(&Some(r.clone()), &Node::new(item, None, None)),
             None    => Node::new(item, None, None)
@@ -20,6 +24,18 @@ impl SkewHeap {
         self.items += 1;
 
         return self.items
+    }
+
+    pub fn get(&mut self) -> Option<Item> {
+        return match &self.root {
+            None => None,
+            Some(r) => {
+                self.items -= 1;
+                let item = r.item;
+                self.root = Node::merge(&r.left, &r.right);
+                Some(item)
+            }
+        }
     }
 }
 
@@ -46,5 +62,36 @@ impl Node {
             (Some(a), Some(b)) if a.item > b.item => Node::merge(&Some(b.clone()), &Some(a.clone())),
             (Some(a), Some(b))                    => Node::new(a.item, Node::merge(&a.right, &Some(b.clone())).clone(), a.left.clone()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SkewHeap;
+
+    #[test]
+    fn test_ordering() {
+        let mut skew = SkewHeap::new();
+
+        assert!(skew.is_empty());
+
+        skew.put(10);
+        skew.put(3);
+        skew.put(15);
+
+        assert_eq!(skew.items, 3);
+        assert!(!skew.is_empty());
+
+        assert_eq!(skew.get(), Some(3));
+        assert_eq!(skew.items, 2);
+        assert!(!skew.is_empty());
+
+        assert_eq!(skew.get(), Some(10));
+        assert_eq!(skew.items, 1);
+        assert!(!skew.is_empty());
+
+        assert_eq!(skew.get(), Some(15));
+        assert_eq!(skew.items, 0);
+        assert!(skew.is_empty());
     }
 }
