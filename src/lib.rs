89bfc77dd838d1e5a577f5708d5eeee174c0e4f4
Jeff Ober <jober@ziprecruiter.com>
2021-12-08 17:06:29 -0500

    Replace nested structs with indices to structs stored in vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,138 +1,118 @@
 //! A mergeable priority heap
-use std::collections::VecDeque;
 
-/// Parameterizes the SkewHeap. Items stored in the heap are prioritized in ascending order.
+/*
+ * TODO:
+ *      -garbage-collect dead nodes out of the vector
+ *      -store ref to item so it needn't be copied
+ */
+
 pub trait Item: Ord + Copy {}
 impl<T: Ord + Copy> Item for T {}
 
+type Index = Option<usize>;
 
-type BoxedNode<T> = Box<Node<T>>;
-type Tree<T> = Option<BoxedNode<T>>;
-
-
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 struct Node<T> {
-    item:  T,
-    left:  Tree<T>,
-    right: Tree<T>,
+    item:  Option<T>,
+    left:  Index,
+    right: Index,
 }
 
 impl<T: Item> Node<T> {
-    fn new(item: T, left: Tree<T>, right: Tree<T>) -> Tree<T> {
-        Some(Box::new(Node{ item, left, right }))
-    }
-
-    fn merge (a: Tree<T>, b: Tree<T>) -> Tree<T> {
-        let mut queue: VecDeque<BoxedNode<T>> = VecDeque::new();
-        let mut trees: VecDeque<BoxedNode<T>> = VecDeque::new();
-
-        if let Some(a) = a {
-            queue.push_back(a);
+    fn new(item: Option<T>) -> Node<T> {
+        Node{
+            item:  item,
+            left:  None,
+            right: None,
         }
-
-        if let Some(b) = b {
-            queue.push_back(b);
-        }
-
-        // Cut right subtrees from each path
-        while queue.len() > 0 {
-            if let Some(mut node) = queue.pop_front() {
-                // Remove the right node and add it to the queue, if present.
-                if let Some(right) = node.right {
-                    queue.push_back(right);
-                    node.right = None;
-                }
-
-                // Add the node to the list of cut nodes
-                trees.push_back(node);
-            }
-        }
-
-        // Sort the collected subtrees
-        trees.make_contiguous().sort();
-
-        // Reduce right by popping off the back of the list, using the final/ultimate node as our
-        // accumulator, merging the ultimate node into the penultimate node until there is only
-        // one left.
-        while trees.len() > 1 {
-            if let Some(ult) = trees.pop_back() {
-                if let Some(mut penult) = trees.pop_back() {
-                    // Swap the left and right if the penultimate node has a left subtree. Its
-                    // right subtree has already been cut.
-                    if let Some(left) = penult.left {
-                        penult.right = Some(left);
-                    }
-
-                    // Set the penultimate's left child to be the previous ultimate
-                    penult.left = Some(ult);
-
-                    // Now make the penultimate be the ultimate node in the list
-                    trees.push_back(penult);
-                }
-            }
-        }
-
-        trees.pop_front()
     }
 }
 
-
 /// A skew heap is an unbounded priority (min) heap. It is paramaterized by the type of item to be
 /// stored in it. Items must implement PartialOrd and Clone.
-pub struct SkewHeap<T: Item> {
-    size: u64,
-    root: Tree<T>,
+pub struct SkewHeap<T> {
+    count: usize,
+    root:  Index,
+    nodes: Vec<Node<T>>,
 }
 
 impl<T: Item> SkewHeap<T> {
     /// Returns a new SkewHeap
-    pub fn new() -> SkewHeap<T> {
-        SkewHeap{
-            size: 0,
-            root: None,
+    pub fn new() -> Self {
+        Self {
+            count: 0,
+            root:  None,
+            nodes: Vec::new(),
         }
     }
 
     /// Returns the number of items in the SkewHeap
-    pub fn size(&self) -> u64 {
-        return self.size
+    pub fn size(&self) -> usize {
+        self.count
     }
 
     /// Returns true if there are no items currently in the SkewHeap
     pub fn is_empty(&self) -> bool {
-        return self.size == 0
+        return self.count == 0
     }
 
     /// Inserts an item into the heap and returns the new size
-    pub fn put(&mut self, item: T) -> u64 {
-        self.root = match &self.root {
-            None    => Node::new(item, None, None),
-            Some(r) => Node::merge(Some(r.clone()), Node::new(item, None, None)),
-        };
+    pub fn put(&mut self, item: T) -> usize {
+        self.count += 1;
+        let node = self.alloc_node(item);
 
-        self.size += 1;
+        if self.count == 1 {
+            self.root = node;
+        } else {
+            self.root = self.merge(self.root, node);
+        }
 
-        return self.size
+        self.count
     }
 
     /// Removes and retrieves the top item from the heap
     pub fn take(&mut self) -> Option<T> {
-        return match &self.root {
-            None    => None,
-            Some(r) => {
-                self.size -= 1;
-                let item = r.item;
-                self.root = Node::merge(r.left.clone(), r.right.clone());
-                Some(item)
-            }
+        if self.count == 0 {
+            return None
+        }
+
+        if let Some(root) = self.root {
+            let item  = self.nodes[root].item;
+            let left  = self.nodes[root].left;
+            let right = self.nodes[root].right;
+
+            self.count -= 1;
+            self.root = self.merge(left, right);
+            item
+        } else {
+            None
         }
     }
 
     /// Retrieves the top item from the heap without removing it
     pub fn peek(&self) -> Option<T> {
-        return match &self.root {
+        match self.root {
             None    => None,
-            Some(r) => Some(r.item),
+            Some(n) => self.nodes[n].item,
+        }
+    }
+
+    fn alloc_node(&mut self, item: T) -> Index {
+        self.nodes.push(Node::new(Some(item)));
+        Some(self.nodes.len() - 1)
+    }
+
+    fn merge(&mut self, a: Index, b: Index) -> Index {
+        match (a, b) {
+            (None,    None)                                               => None,
+            (Some(a), None)                                               => Some(a),
+            (None,    Some(b))                                            => Some(b),
+            (Some(a), Some(b)) if self.nodes[a].item > self.nodes[b].item => self.merge(Some(b), Some(a)),
+            (Some(a), Some(b))                                            => {
+                let tmp = self.nodes[a].right;
+                self.nodes[a].right = self.nodes[a].left;
+                self.nodes[a].left = self.merge(Some(b), tmp);
+                Some(a)
+            },
         }
     }
 }
