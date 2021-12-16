c53f1c277242e3c96ef9cfc92d92d61707bd696e
Jeff Ober <jober@ziprecruiter.com>
2021-12-16 14:56:15 -0500

    simplify benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -4,53 +4,14 @@ use rand::seq::SliceRandom;
 
 use skewheap::SkewHeap;
 
-fn make_filled_skewheap(size: usize) -> SkewHeap<u32> {
-    let mut s = SkewHeap::new();
-    for n in 0..size {
-        s.put(n as u32);
-    }
-
-    s
-}
-
-fn put(c: &mut Criterion) {
-    let mut group = c.benchmark_group("put into skewheap of size");
-    let sizes = [0, 10, 50, 100, 500, 1000];
-
-    for size in sizes {
-        group.throughput(Throughput::Elements(size as u64));
-        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-            let mut s = make_filled_skewheap(size);
-            b.iter(|| s.put(42 as u32))
-        });
-    }
-
-    group.finish();
-}
-
-fn take(c: &mut Criterion) {
-    let mut group = c.benchmark_group("take from skewheap of size");
-    let sizes = [0, 10, 50, 100, 500, 1000];
-
-    for size in sizes {
-        group.throughput(Throughput::Elements(size as u64));
-        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-            let mut s = make_filled_skewheap(size);
-            b.iter(|| s.take())
-        });
-    }
-
-    group.finish();
-}
-
 fn fill_drain(c: &mut Criterion) {
     let mut group = c.benchmark_group("fill then drain skewheap of size");
     let counts = [10, 50, 100, 500];
 
-    for count in counts {
-        group.throughput(Throughput::Elements(count as u64));
-        group.bench_with_input(BenchmarkId::from_parameter(count), &count, |b, &count| {
-            let mut items: Vec<u32> = (0..count).collect();
+    for count in counts.iter() {
+        group.throughput(Throughput::Elements(*count as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(*count), &count, |b, &count| {
+            let mut items: Vec<u32> = (0..*count).collect();
             items.shuffle(&mut thread_rng());
 
             b.iter(|| {
@@ -70,5 +31,5 @@ fn fill_drain(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, put, take, fill_drain);
+criterion_group!(benches, fill_drain);
 criterion_main!(benches);
